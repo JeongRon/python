7cf6d63085940f39f39bbe1047dbe36e91d84e57
이정론 <83465749+JeongRon@users.noreply.github.com>
2022-08-27 23:48:52 +0900

    easy

---
+++ not_class/n4101.py
@@ -0,0 +1,12 @@
+'''
+4101 - 크냐? (bronze 5)
+'''
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    else:
+        if a > b:
+            print('Yes')
+        else:
+            print('No')
