46a5f3e7532f149eb25829ec54070fd0f352b3fd
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-10-14 19:32:17 +0900

    동빈나_이코테 / 4. 정렬 알고리즘

--- coding_test/sorting/sorting_2.py
+++ coding_test/sorting/sorting_2.py
@@ -1,5 +1,6 @@
 """
 삽입 정렬
+: 처리되지 않은 데이터를 하나씩 골라 적절한 위치에 삽입
 - 평균 시간 복잡도 : O(N²)
 - 공간 복잡도 : O(N)
 - 선택 정렬과 시간 복잡도 똑같지만,
@@ -10,10 +11,10 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
 for i in range(1, len(array)):
-    for j in range(i, 0, -1):
-        if array[j] < array[j - 1]:
+    for j in range(i, 0, -1):  # 인덱스 i부터 1까지 1씩 감소하며 반복
+        if array[j] < array[j - 1]:  # 한 칸씩 왼쪽으로 이동
             array[j], array[j - 1] = array[j - 1], array[j]
-        else:
+        else:  # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
             break
 
 print(array)
