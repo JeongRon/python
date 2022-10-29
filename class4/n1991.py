c778e5d0e7d788a2193d755a4111e749d659418f
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-10-29 13:54:21 +0900

    class4 / 1991 - 트리 순회 / silver 1 / 트리, 재귀

--- class4/n1991.py
+++ class4/n1991.py
@@ -1,13 +1,42 @@
 """
-1991 - 트리 순회 / silver 1 /
+1991 - 트리 순회 / silver 1 / 트리, 재귀
 """
 import sys
 
 input = sys.stdin.readline
 
-tree = []
+tree = {}
 N = int(input().rstrip())
 for _ in range(N):
-    tree.append(list(input().split()))
+    root, left, right = input().split()
+    tree[root] = [left, right]
 
-print(tree)
+# 전위
+def preorder(node):
+    if node != ".":
+        print(node, end="")
+        preorder(tree[node][0])
+        preorder(tree[node][1])
+
+
+# 중위 inorder
+def inorder(node):
+    if node != ".":
+        inorder(tree[node][0])
+        print(node, end="")
+        inorder(tree[node][1])
+
+
+# 후위 postorder
+def postorder(node):
+    if node != ".":
+        postorder(tree[node][0])
+        postorder(tree[node][1])
+        print(node, end="")
+
+
+preorder("A")
+print()
+inorder("A")
+print()
+postorder("A")
