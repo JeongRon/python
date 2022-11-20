b3bc355f6b619b39d6c50f2687b4c45000afa915
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-11-20 22:57:23 +0900

    easy

---
+++ not_class/n14681.py
@@ -0,0 +1,14 @@
+"""
+14681 - 사분면 고르기 / bronze 5
+"""
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+else:
+    print(4)
