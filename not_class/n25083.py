ec86794119694a084a9feaa6ceb159b2faa31e8e
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-11-26 23:13:31 +0900

    easy

---
+++ not_class/n25083.py
@@ -0,0 +1,16 @@
+"""
+25083 - 새싹 / bronze 5
+"""
+print("         ,r'", end="")
+print('"7')
+
+print("r`-_   ,'  ,/")
+
+print(' \. "', end="")
+print(". L_r'")
+
+print("   `~\/")
+
+print("      |")
+
+print("      |")
